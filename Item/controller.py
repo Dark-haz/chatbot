ba75d64dc760b781f8439b5ad55459d8a0beecd0
MG <dubbzi321@gmail.com>
2024-09-10 11:23:26 +0300

    renewed databae pass

--- Item/controller.py
+++ Item/controller.py
@@ -16,7 +16,7 @@ def execution():
     similar_items = item_repository.find_KNN(user_input_embed,10)
 
     product_selection_prompt= recommend_product_prompt(structured_user_input,similar_items)
-    result=invoke_bedrock_claude(product_selection_prompt)
+    result=invoke_bedrock_claude(product_selection_prompt , 500)
     return result
     
     
--- config/static.py
+++ config/static.py
@@ -1,3 +1,3 @@
-DATABASE_URL = "postgresql://postgres:OHoxyVgCqADVwQiMqBwu@database-1.czykooyqkq7y.us-east-1.rds.amazonaws.com:5432/postgres"
+DATABASE_URL = "postgresql://postgres:KfNkamc7uqlIdH5rsxz8@database-1.czykooyqkq7y.us-east-1.rds.amazonaws.com:5432/postgres"
 
 VECTOR_DIMENSION = 256
